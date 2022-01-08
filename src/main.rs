2388fd83ef3407c6a3a251b653472aae4e58c6b2
jiko <j1ko@abv.bg>
2022-01-08 12:04:07 +0200

    [test] now works with rectangles nad squares

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,8 @@ impl Renderer {
 
 
         // get the canvas
-        let mut ekran = window.into_canvas().build().unwrap();
-        ekran.set_logical_size(1280, 720).unwrap();
+        let ekran = window.into_canvas().build().unwrap();
+        // ekran.set_logical_size(1280, 720).unwrap();
 
 
         Renderer {
